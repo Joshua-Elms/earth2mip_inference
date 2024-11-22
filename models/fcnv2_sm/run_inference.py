6bcf2697001ec33f1f7bd839540191c30aecbec9
Joshua Elms <joshua.elms111@gmail.com>
2024-11-21 19:24:23 -0500

    testing inf

--- models/fcnv2_sm/run_inference.py
+++ models/fcnv2_sm/run_inference.py
@@ -1,21 +1,26 @@
 from inference_utils import run_custom_inference, run_simple_inference
 import datetime
+from time import perf_counter
 
 # make sure to set the environment variable MODEL_REGISTRY before running this script
 # see documentation in the README and the docstrings for both of these functions for more information
-# run_custom_inference(
-#     data_source_path="/N/slate/jmelms/projects/FCN_dynamical_testing/data/initial_conditions/processed_ic_sets/default/",
-#     output_path="/N/slate/jmelms/projects/FCN_dynamical_testing/data/output/ideal_default_60t.nc",
-#     n_iters=60,
-#     start_time=datetime.datetime(1970, 1, 1),
-#     device="cpu",
-#     vocal=True
-# )
-
-run_simple_inference(
-    output_path="/N/slate/jmelms/projects/FCN_dynamical_testing/data/output/kwesi_test.nc",
-    n_iters=500,
-    start_time=datetime.datetime(2010, 1, 1),
+start = perf_counter()
+run_custom_inference(
+    data_source_path="/N/slate/jmelms/projects/FCN_dynamical_testing/data/initial_conditions/processed_ic_sets/default/",
+    output_path="/N/slate/jmelms/projects/FCN_dynamical_testing/data/output/ideal_default_1460t.nc",
+    n_iters=1460,
+    start_time=datetime.datetime(1970, 1, 1),
     device="cpu",
     vocal=True
 )
+
+# run_simple_inference(
+#     output_path="/N/slate/jmelms/projects/FCN_dynamical_testing/data/output/kwesi_test.nc",
+#     n_iters=500,
+#     start_time=datetime.datetime(2010, 1, 1),
+#     device="cpu",
+#     vocal=True
+# )
+stop = perf_counter()
+
+print(f"Inference completed in: {stop-start:.2f} seconds")
--- models/fcnv2_sm/run_inference.sh
+++ models/fcnv2_sm/run_inference.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 #SBATCH -J fcnv2_sm_inference
-#SBATCH -p debug
+#SBATCH -p general
 #SBATCH -o output.out
 #SBATCH -e log.err
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=128
-#SBATCH --time=01:00:00
-#SBATCH --mem=16GB
+#SBATCH --time=06:00:00
+#SBATCH --mem=200GB
 #SBATCH -A r00389
+
+export MODEL_REGISTRY="/N/u/jmelms/BigRed200/projects/earth2mip_inference/models"
 
 source activate ~/envs/earth2mip
 
